026b95d42ebfb8c99292ac72ef9aae0d1868fd72
Afonso Duarte <afonsomartins@gmail.com>
2015-10-09 12:26:22 +0100

    Foundation static site template

---
+++ README.md
@@ -0,0 +1,11 @@
+# Foundation static site #
+
+Run 
+
+    npm install
+
+then
+    
+    gulp
+
+to get up and running.
---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+var $ = jQuery = require('jquery');
+var waypoints  = require('waypoints');
+
+// Call buy now pop up
+$('map area').click(function () {
+  console.log(this.className);
+})
